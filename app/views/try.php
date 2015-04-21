5ea3ba74c95a85222f17eb6d05bc6e50efe24fe3
stefobark <steve@sphinxsearch.com>
2015-04-21 07:46:18 -0700

    organized some stuff

--- app/views/try.php
+++ app/views/try.php
@@ -1,41 +1,30 @@
 <html>
    <head>
-
       <link href="/css/tweaks2.css" rel="stylesheet">
-            <link href="/css/bootstrap.css" rel="stylesheet">
+      <link href="/css/bootstrap.css" rel="stylesheet">
       <script src="/js/jquery-2.1.1.min.js"></script>
-      
-       <script src="/js/jquery.alterclass.js"></script>
-		<script src='/js/spectrum.js'></script>
-		<link rel='stylesheet' href='/css/spectrum.css' />
+      <script src="/js/jquery.alterclass.js"></script>
+      <script src='/js/spectrum.js'></script>
+      <link rel='stylesheet' href='/css/spectrum.css' />
       <script type="text/javascript" src="/js/jquery.func_toggle.js"></script>
       <script src="/js/try.js"></script>
-      
-
-    </head>
-    <body>
-   <div style="position:fixed; top:10px; left:150px;">
-   	<img src="/img/logo.png" style="width:200px">
-   </div>
-   
-    <div class="container">
-    <div class="row">
-    <div class="col-md-12">
-		<div class="choose" style="position: fixed; top: 40px; left: 280px;">
-			<p class="help-block">Numbers of Warps:
-			</p>
-				<input type="text" class="warpNum"><br>
-			<p class="help-block">Number of Wefts:</p>
-				<input type="text" class="weftNum" style="margin-left:1px"><br>
-				<p class="help-block">Number of Treadles:</p>
-				<input type="text" class="treadleNum" style="margin-left:1px"><br><br>
-
-			<button class="submit">Go</button>
-
-	<script>
-		
-	</script>
-		</div>
-    		
-    		
-</body>
+   </head>
+   <body>
+      <div style="position:fixed; top:10px; left:150px;">
+         <img src="/img/logo.png" style="width:200px">
+      </div>
+      <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+      <div class="choose" style="position: fixed; top: 40px; left: 280px;">
+         <p class="help-block">Numbers of Warps:
+         </p>
+         <input type="text" class="warpNum"><br>
+         <p class="help-block">Number of Wefts:</p>
+         <input type="text" class="weftNum" style="margin-left:1px"><br>
+         <p class="help-block">Number of Treadles:</p>
+         <input type="text" class="treadleNum" style="margin-left:1px"><br><br>
+         <button class="submit">Go</button>
+         <script></script>
+      </div>
+   </body>
